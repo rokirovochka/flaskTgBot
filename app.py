6e6f7790bf808ecf16b4f40842f607f2564694e2
Nikita Maksakovsky <nmaksakovskii@gmail.com>
2023-12-19 22:24:24 +0700

    weather_addings

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-#!/usr/bin/env python
+
 import requests
 import time
 from flask import Flask, request
 import os
 
 TOKEN = os.environ.get('TOKEN')
+WEATHER_TOKEN = os.environ.get('WEATHER_TOKEN')
 requests.post(f"https://api.telegram.org/bot{TOKEN}/setWebhook" , json={'url': 'https://flask-tg-bot.onrender.com'})
 
+params = {
+  'access_key': WEATHER_TOKEN,
+  'query': 'Novosibirsk'
+}
 
 app = Flask(__name__)
 def send_message(chat_id,text):
@@ -18,9 +23,12 @@ def send_message(chat_id,text):
 @app.route("/", methods=['GET', 'POST'])
 def receive_update():
     if request.method == 'POST':
-        print(request.json)
+        #print(request.json)
         chat_id = request.json['message']['chat']['id']
-        send_message(chat_id, 'pong')
+	api_result = requests.get('http://api.weatherstack.com/current', params)
+	api_response = api_result.json()
+	text = u'Current temperature in %s is %d℃' % (api_response['location']['name'], api_response['current']['temperature'])
+	send_message(chat_id, text)
     return {'ok':True}
 
 
