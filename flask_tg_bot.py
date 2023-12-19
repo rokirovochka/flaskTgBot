14c5cd58f4def25c2391aaf7379a4e749f79f4cc
Nikita Maksakovsky <nmaksakovskii@gmail.com>
2023-12-19 21:43:32 +0700

    pong commit

---
+++ flask_tg_bot.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import requests
+import time
+from flask import Flask, request
+
+import mySecrets
+
+TOKEN = mySecrets.TOKEN
+#app_url = mySecrets.app_url
+
+
+requests.post(f"https://api.telegram.org/bot{TOKEN}/setWebhook" , json={'url': 'https://flask-tg-bot.onrender.com'})
+
+
+app = Flask(__name__)
+def send_message(chat_id,text):
+    method = "sendMessage"
+    url = f"https://api.telegram.org/bot{TOKEN}/{method}"
+    data = {'chat_id':chat_id, 'text':text}
+    requests.post(url, data=data)
+    
+@app.route("/", methods=['GET', 'POST'])
+def receive_update():
+    if request.method == 'POST':
+        print(request.json)
+        chat_id = request.json['message']['chat']['id']
+        send_message(chat_id, 'pong')
+    return {'ok':True}
+
+
+
+
